8c013696a88722983c503e7e82a1e4c8e586b500
kazarigian <118138326+kazarigian@users.noreply.github.com>
2023-10-11 04:00:47 -0400

    restart-add

---
+++ src/main/java/com/mycompany/databaseexample/Song.java
@@ -0,0 +1,56 @@
+package com.mycompany.databaseexample;
+
+public class Song {
+
+    private int id;
+    private String title;
+    private String artist;
+
+    Song(int id, String title, String artist) {
+
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the artist
+     */
+    public String getArtist() {
+        return artist;
+    }
+
+    /**
+     * @param artist the artist to set
+     */
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+}
