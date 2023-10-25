819c2bfc7370af69fdd67fadd0d32958f94978c7
erickH1999 <erickh2013@icloud.com>
2023-10-25 16:06:08 -0400

    Updated the update function

--- src/main/java/com/mycompany/databaseexample/Artist.java
+++ src/main/java/com/mycompany/databaseexample/Artist.java
@@ -9,5 +9,52 @@ package com.mycompany.databaseexample;
  * @author erick
  */
 public class Artist {
+ 
+    public int id;
+    public String artist_name;
+    public String genre;
+    public String song;
+    
+    public Artist(int id, String artist_name, String genre, String song) {
+        this.id = id;
+        this.artist_name = artist_name;
+        this.genre = genre;
+        this.song = song;    
+    }
+    
+    public int getID() {
+        return id;
+    }
+    
+    public void setID(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return artist_name;
+    }
+    
+    public void setName(String artist_name) {
+        this.artist_name = artist_name; 
+    }
+    
+    public String getGenre() {
+        return genre;
+    }
+    
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    public String getSong() {
+        return song;
+    }
+    
+    public void setSong(String song) {
+        this.song = song;
+    }
+    
+    
+    
     
 }
